1056f8fd4d79c8293c09caa00dfb4fb8963b14e7
Sudeep Srivastava <susrivastava@groupon.com>
2020-02-19 19:26:49 +0000

    Adding file reader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        try (Stream<String> stream = Files.lines(Paths.get("text"))) {
+            stream.forEach(System.out::println);
+        }
+    }
 }
